893048a7c091b4eee30c8cb596e37884f06b9274
faizan-523 <faizan.7.ist@gmail.com>
2025-05-26 22:08:31 +0500

    Simple code of Basic Calculator

---
+++ Basic Caluclator.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter a 1st number: "))
+num2 = int(input("enter a 2nd number: "))
+
+print("Sum of numbers is: ",num1 + num2)
+print("Subtraction of numbers is: ",num1 - num2)
+print("Product of numbers is: ",num1*num2)
+print("Division of numbers is: ",num1/num2)
+print("Modulus of numbers is : ",num1%num2)
