f96250b01d0e5f49542b98ff6aeed04c99af5337
faizan-523 <faizan.7.ist@gmail.com>
2025-05-29 23:58:16 +0500

    Lists in python

---
+++ lists.py
@@ -0,0 +1,22 @@
+marks=[23,57,91,5]
+print(marks)
+print(marks[0])
+print(marks[3])
+
+student=["Faizan", 23, "CS"]
+print(student)
+print(student[0:2])
+
+print(len(student))
+
+marks.append(20)
+print(marks)
+
+marks.sort()
+print(marks)
+
+marks.sort(reverse=True)
+print(marks)
+
+student.reverse()
+print(student)
