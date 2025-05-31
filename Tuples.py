33a16b37ab84b2f992ae498ca92ef052783bb9f9
faizan-523 <faizan.7.ist@gmail.com>
2025-05-31 22:20:31 +0500

    Tuple in python

--- Tuples.py
+++ Tuples.py
@@ -1,4 +1,12 @@
-grade=["A", "B", "C", "A", "A"]
-grade.sort()
+grade=("A", "B", "C", "A", "A")
 print(grade)
 
+#tuple is Immutable we can not append sort or something etc  if we want to do something 
+#first we should convert tuple into list
+
+grade_list = list(grade)
+grade_list.append("B")
+
+grade_list.sort()
+
+print("Updated & Sorted Grades:", grade_list)
