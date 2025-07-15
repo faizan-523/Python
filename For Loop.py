af90a277514d8736d35e72af584719f90cc862f1
faizan-523 <faizan.7.ist@gmail.com>
2025-07-15 12:01:09 +0500

    For Loop

---
+++ For Loop.py
@@ -0,0 +1,10 @@
+print("For Loop in Python")
+
+print("Printing table of number which user will enter")
+
+print("Enter a number:")
+num = int(input())
+print("Table of", num, "is:")
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
+
