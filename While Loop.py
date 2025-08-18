fcd51b8b53392b6e1e310e4e893e68cccd21625c
faizan-523 <faizan.7.ist@gmail.com>
2025-08-18 12:02:18 +0500

    While Loop in python

--- While Loop.py
+++ While Loop.py
@@ -2,12 +2,12 @@ print("While Loop in Python")
 
 print("Printing numbers from 1 to 100 using while loop")
 i=1
-while i<=100:
+while i<=100: #if condition is true, it will execute this block of code
     print(i)
     i +=1
 
 print("Printing the square of number using while loop")
 i = 1
 while i <= 10:
-    print(f"The square of {i} is :", i** 2)
+    print(f"The square of {i} is :", i** 2) #it will print the square of number
     i += 1
