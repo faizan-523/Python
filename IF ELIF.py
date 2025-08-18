79c9a9f7967ddfdcc9127e14269a3f1e2ccb0e34
faizan-523 <faizan.7.ist@gmail.com>
2025-08-18 12:00:16 +0500

    IF ELIF in python

--- IF ELIF.py
+++ IF ELIF.py
@@ -1,5 +1,10 @@
 marks= int(input("enter yout marks: "))
 
+# This program calculates the grade based on marks using if-elif-else statements
+# It assigns grades A, B, C, D, or F based on the marks obtained
+# In IF ELIF statements, the first condition that is true will be executed
+# If no conditions are true, the else block will be executed
+
 if(marks >= 90):
     grade="A"
 elif(marks >= 80 and marks<90):
