39db078633571875b75f385b00a8912126929ee9
faizan-523 <faizan.7.ist@gmail.com>
2025-08-18 11:53:10 +0500

    Strings

--- Strings.py
+++ Strings.py
@@ -1,7 +1,9 @@
+#in String we can store both integer and character and both at same time
+
 str1= "My name is "
 str2= "Faizan"
 str3= str1+str2
 print(str3)
 print(len(str3))
-print(str2[ :5])
+print(str2[ :4])
 print(str1[3:6])
