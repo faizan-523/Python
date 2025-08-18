e23e307ad4a5fa4ba1d607a45591fc8ffb4a4e51
faizan-523 <faizan.7.ist@gmail.com>
2025-08-18 12:04:19 +0500

    For Loop in Python

--- For Loop.py
+++ For Loop.py
@@ -3,8 +3,10 @@ print("For Loop in Python")
 print("Printing table of number which user will enter")
 
 print("Enter a number:")
-num = int(input())
+num = int(input()) #it will take input from user and convert it to integer
 print("Table of", num, "is:")
+# For Loop in Python to print the table of a number
+# The loop will run from 1 to 10
 for i in range(1, 11):
     print(num, "x", i, "=", num * i)
 
