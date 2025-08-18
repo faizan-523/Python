c91ebcde78a26613cd493c14180b18232eb64d59
faizan-523 <faizan.7.ist@gmail.com>
2025-08-18 11:57:34 +0500

    Dictionary in python

--- Dictionary.py
+++ Dictionary.py
@@ -5,6 +5,10 @@ info= {
     "Marks": [90, 87, 93, 79],
     "CGPA": 3.5
 }
+#in dictionary we can store string lists and integer also at same time
+# Dictionary is a collection of key-value pairs
+# In dictionary on left side we have key and on right side we have value
+# Key is unique and value can be anything like string, integer, list, etc.
 
 print(info)
 print(type(info))
