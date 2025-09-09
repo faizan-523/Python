d84a066437cf638afd27d8e0281e389eedbf356f
faizan-523 <faizan.7.ist@gmail.com>
2025-09-09 12:51:35 +0500

    For Loop

--- For Loop.py
+++ For Loop.py
@@ -3,11 +3,17 @@
 
 print("Printing table of number which user will enter")
 
-print("Enter a number:")
-num = int(input()) #it will take input from user and convert it to integer
+num = int(input("Enter a number: "))
 print("Table of", num, "is:")
 # For Loop in Python to print the table of a number
 # The loop will run from 1 to 10
 for i in range(1, 11):
     print(num, "x", i, "=", num * i)
+
+#Example 2: Iterating through a list
+print("\nIterating through a list of fruits:")
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
 
