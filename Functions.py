5e131d654b31a9b4c6895c36359360f5f9a0fd14
faizan-523 <faizan.7.ist@gmail.com>
2025-09-09 12:11:00 +0500

    Functions in Python

---
+++ Functions.py
@@ -0,0 +1,17 @@
+#Functions in Python
+# A function is a block of code which only runs when it is called.
+# You can pass data, known as parameters, into a function.
+
+def sum(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("The sum is:", sum(num1, num2))
+
+# You can also define a function without parameters
+def hello():
+    print("Hello, World!")
+
+hello() #it will print Hello, World! because we called the function
