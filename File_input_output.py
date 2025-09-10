85ad26803e6377c658cecc1fbfa94c9ad0c2ee0b
faizan-523 <faizan.7.ist@gmail.com>
2025-09-10 11:49:16 +0500

    File Handling

--- File_input_output.py
+++ File_input_output.py
@@ -13,6 +13,21 @@ print(len(data)) #it print length of data
 #Once we read all lines, the cursor is at the end of the file 
 #After reading, if we try to read again, it will return an empty string
 
+file.close()
 
+#Writing to a file
+file = open('demo.txt', 'a')  # 'a' for append mode
+#Append means it will add data at the end of the file 
+#we should use 'w' mode to overwrite the file but it will delete all previous data
 
+file.write("\n Now i am doing file handling in python")
+
+file.close()
+
+#We can also write and read from a file simultaneously using 'r+' or 'w+' or 'a+' modes
+file = open('demo.txt', 'r+')
+value= file.read()
+print(value)
+
+file.write("\n Python is great language")
 file.close()
